f55a0b5cbe815f68eb264a4efd5bc85d1987fbf1
ncxton <ngct@tuta.io>
2026-06-26 11:00:50 +0700

    fix(cmd): models cmd now prints static list

--- internal/cli/models_cmd.go
+++ internal/cli/models_cmd.go
@@ -11,7 +11,6 @@ import (
 	"github.com/ncxton/potaco/internal/auth"
 	"github.com/ncxton/potaco/internal/config"
 	"github.com/ncxton/potaco/internal/credential"
-	"github.com/ncxton/potaco/internal/tui"
 	"github.com/spf13/cobra"
 )
 
@@ -90,10 +89,6 @@ func runModels(cmd *cobra.Command, args []string) error {
 	}
 
 	jsonMode, _ := cmd.Root().PersistentFlags().GetBool("json")
-
-	if !jsonMode && tui.IsInteractive() {
-		return tui.RunModelList(providerName, apiKey)
-	}
 
 	models, err := ad.DiscoverModels(context.Background())
 	if err != nil {
